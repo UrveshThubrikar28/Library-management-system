66643682402393e8256f3fd2ac613eae393d4ca9
Uvesh Sanjay Thubrikar <142099384+UrveshThubrikar28@users.noreply.github.com>
2025-05-30 08:07:44 +0530

    Update Library_Management.c

--- Library_Management.c
+++ Library_Management.c
@@ -1,142 +1,133 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<time.h>
+#include<conio.h>
 
-struct books{
+struct books {
     int id;
     char bookName[50];
     char authorName[50];
     char date[12];
-}b;
+} b;
 
-struct student{
+struct student {
     int id;
     char sName[50];
     char sClass[50];
     int sRoll;
     char bookName[50];
     char date[12];
-}s;
+} s;
 
 FILE *fp;
 
-int main(){
+// Function declarations
+void addBook();
+void booksList();
+void del();
+void issueBook();
+void issueList();
 
+int main() {
     int ch;
 
-    while(1){
+    while (1) {
         system("cls");
         printf("<== Library Management System ==>\n");
-        printf("1.Add Book\n");
-        printf("2.Books List\n");
-        printf("3.Remove Book\n");
-        printf("4.Issue Book\n");
-        printf("5.Issued Book List\n");
-        printf("0.Exit\n\n");
+        printf("1. Add Book\n");
+        printf("2. Books List\n");
+        printf("3. Remove Book\n");
+        printf("4. Issue Book\n");
+        printf("5. Issued Book List\n");
+        printf("0. Exit\n\n");
         printf("Enter your choice: ");
         scanf("%d", &ch);
 
-        switch(ch){
-        case 0:
-            exit(0);
-
-        case 1:
-            addBook();
-            break;
-
-        case 2:
-            booksList();
-            break;
-
-        case 3:
-            del();
-            break;
-
-        case 4:
-            issueBook();
-            break;
-
-        case 5:
-            issueList();
-            break;
-
-        default:
-            printf("Invalid Choice...\n\n");
-
+        switch (ch) {
+            case 0:
+                exit(0);
+            case 1:
+                addBook();
+                break;
+            case 2:
+                booksList();
+                break;
+            case 3:
+                del();
+                break;
+            case 4:
+                issueBook();
+                break;
+            case 5:
+                issueList();
+                break;
+            default:
+                printf("Invalid Choice...\n\n");
         }
-        printf("Press Any Key To Continue...");
+        printf("Press any key to continue...");
         getch();
     }
 
     return 0;
 }
 
-
-void addBook(){
+void addBook() {
     char myDate[12];
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    sprintf(myDate, "%02d/%02d/%d", tm.tm_mday, tm.tm_mon+1, tm.tm_year + 1900);
+    sprintf(myDate, "%02d/%02d/%d", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
     strcpy(b.date, myDate);
 
     fp = fopen("books.txt", "ab");
 
     printf("Enter book id: ");
     scanf("%d", &b.id);
+    fflush(stdin);
 
     printf("Enter book name: ");
-    fflush(stdin);
-    gets(b.bookName);
+    fgets(b.bookName, sizeof(b.bookName), stdin);
+    b.bookName[strcspn(b.bookName, "\n")] = 0;
 
     printf("Enter author name: ");
-    fflush(stdin);
-    gets(b.authorName);
-
-    printf("Book Added Successfully");
+    fgets(b.authorName, sizeof(b.authorName), stdin);
+    b.authorName[strcspn(b.authorName, "\n")] = 0;
 
     fwrite(&b, sizeof(b), 1, fp);
     fclose(fp);
+
+    printf("Book Added Successfully\n");
 }
 
-
-void booksList(){
-
+void booksList() {
     system("cls");
     printf("<== Available Books ==>\n\n");
     printf("%-10s %-30s %-20s %s\n\n", "Book id", "Book Name", "Author", "Date");
 
     fp = fopen("books.txt", "rb");
-    while(fread(&b, sizeof(b), 1, fp) == 1){
+    while (fread(&b, sizeof(b), 1, fp) == 1) {
         printf("%-10d %-30s %-20s %s\n", b.id, b.bookName, b.authorName, b.date);
     }
-
     fclose(fp);
 }
 
-void del(){
-    int id, f=0;
+void del() {
+    int id, f = 0;
+    FILE *ft;
     system("cls");
     printf("<== Remove Books ==>\n\n");
     printf("Enter Book id to remove: ");
     scanf("%d", &id);
 
-    FILE *ft;
-
     fp = fopen("books.txt", "rb");
     ft = fopen("temp.txt", "wb");
 
-    while(fread(&b, sizeof(b), 1, fp) == 1){
-        if(id == b.id){
-            f=1;
-        }else{
+    while (fread(&b, sizeof(b), 1, fp) == 1) {
+        if (id == b.id) {
+            f = 1;
+        } else {
             fwrite(&b, sizeof(b), 1, ft);
         }
-    }
-
-    if(f==1){
-        printf("\n\nDeleted Successfully.");
-    }else{
-        printf("\n\nRecord Not Found !");
     }
 
     fclose(fp);
@@ -145,72 +136,70 @@ void del(){
     remove("books.txt");
     rename("temp.txt", "books.txt");
 
+    if (f == 1) {
+        printf("\n\nDeleted Successfully.\n");
+    } else {
+        printf("\n\nRecord Not Found!\n");
+    }
 }
 
-
-void issueBook(){
-
+void issueBook() {
     char myDate[12];
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    sprintf(myDate, "%02d/%02d/%d", tm.tm_mday, tm.tm_mon+1, tm.tm_year + 1900);
+    sprintf(myDate, "%02d/%02d/%d", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
     strcpy(s.date, myDate);
 
-    int f=0;
-
+    int f = 0;
     system("cls");
     printf("<== Issue Books ==>\n\n");
-
     printf("Enter Book id to issue: ");
     scanf("%d", &s.id);
 
-    //Check if we have book of given id
     fp = fopen("books.txt", "rb");
-
-    while(fread(&b, sizeof(b), 1, fp) == 1){
-        if(b.id == s.id){
+    while (fread(&b, sizeof(b), 1, fp) == 1) {
+        if (b.id == s.id) {
             strcpy(s.bookName, b.bookName);
-            f=1;
+            f = 1;
             break;
         }
     }
+    fclose(fp);
 
-    if(f==0){
+    if (f == 0) {
         printf("No book found with this id\n");
-        printf("Please try again...\n\n");
         return;
     }
 
     fp = fopen("issue.txt", "ab");
+    fflush(stdin);
 
     printf("Enter Student Name: ");
-    fflush(stdin);
-    gets(s.sName);
+    fgets(s.sName, sizeof(s.sName), stdin);
+    s.sName[strcspn(s.sName, "\n")] = 0;
 
     printf("Enter Student Class: ");
-    fflush(stdin);
-    gets(s.sClass);
+    fgets(s.sClass, sizeof(s.sClass), stdin);
+    s.sClass[strcspn(s.sClass, "\n")] = 0;
 
     printf("Enter Student Roll: ");
     scanf("%d", &s.sRoll);
 
-    printf("Book Issued Successfully\n\n");
-
     fwrite(&s, sizeof(s), 1, fp);
     fclose(fp);
+
+    printf("Book Issued Successfully\n\n");
 }
 
-void issueList(){
+void issueList() {
     system("cls");
     printf("<== Book Issue List ==>\n\n");
-
     printf("%-10s %-30s %-20s %-10s %-30s %s\n\n", "S.id", "Name", "Class", "Roll", "Book Name", "Date");
 
     fp = fopen("issue.txt", "rb");
-    while(fread(&s, sizeof(s), 1, fp) == 1){
+    while (fread(&s, sizeof(s), 1, fp) == 1) {
         printf("%-10d %-30s %-20s %-10d %-30s %s\n", s.id, s.sName, s.sClass, s.sRoll, s.bookName, s.date);
     }
-
     fclose(fp);
 }
 
